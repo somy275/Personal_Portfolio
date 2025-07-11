1ccc014f1f85b0d223f9c08f41d1fafb4aaf6283
somy275 <somyranjank@gmail.com>
2025-07-11 13:28:37 +0530

    Some Changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { NavLink } from 'react-router'
 import { FaLinkedinIn } from "react-icons/fa";
 import { BiLogoTwitter } from "react-icons/bi";
@@ -8,8 +8,18 @@ import { IoLogoGithub } from "react-icons/io";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa6";
 import MatterCanvas from '../UI/Matter';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 const Home = () => {
+  const ref = useRef()
+  useGSAP(() => {
 
+    gsap.registerPlugin(gsap);
+    gsap.to(ref.current, {
+      y: 0,
+      stagger: true,
+    })
+  }, {})
   return (
     <>
 
@@ -18,11 +28,14 @@ const Home = () => {
           <span className='h-auto w-fit mb-[2.5rem] mx-auto flex flex-col justify-around gap-[2rem] font-bold '>
             <span className='text-center '>
               <h5 className='text-[max(12px,4.8vw)] min-[450px]:text-[max(20px,4vw)] md:text-[max(27px,3.5vw)] lg:text-[clamp(1rem,0.541rem+0.7172vw,1.4375rem)] text-[#e8eef1]'>Hello, I'm</h5>
-              <h2 className='text-[max(20px,8vw)] min-[450px]:text-[max(35px,7.5vw)] md:text-[max(45px,7vw)]  text-[#43b0f1] lg:text-[clamp(2.5rem,1.9754rem+0.8197vw,3rem)]'>Somyranjan Kumar</h2>
+              <span className="relative h-auto w-auto overflow-hidden flex  ">
+                <h2 ref={ref} className='inline-block text-[max(20px,8vw)] translate-y-[100%] transition-transform duration-[900ms] min-[450px]:text-[max(35px,7.5vw)] md:text-[max(45px,7vw)]  text-[#43b0f1] lg:text-[clamp(2.5rem,1.9754rem+0.8197vw,3rem)]'>Somyranjan Kumar</h2>
+              </span>
+
               <h5 className='text-[max(12px,4.8vw)] min-[450px]:text-[max(20px,4vw)] md:text-[max(27px,3.5vw)] lg:text-[clamp(1rem,0.541rem+0.7172vw,1.4375rem)] text-[#e8eef1]'>Fronted Developer</h5>
             </span>
             <span className='w-auto flex mx-auto gap-[1rem] pointer-events-auto'>
-              <button className='text-[max(10px,4.4vw)] min-[450px]:text-[max(20px,3.7vw)] md:text-[max(25px,3.3vw)]  lg:text-[clamp(1rem,0.541rem+0.7172vw,1.4375rem)] w-[max(7.1rem,38vw)] max-w-[185px] min-[450px]:px-[1rem] md:px-[1.8rem] md:py-[.8rem] lg:py-[.6rem] lg:px-[1.1rem] min-[450px]:max-w-fit lg:w-fit lg:max-w-none relative cursor-pointer border-1 text-[#43b0f1] border-[#43b0f1] py-[0.6rem] rounded-xl overflow-hidden z-[1] before:z-[-1]  before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0  before:bg-[#43b0f1] before:scale-y-0 before:origin-top hover:before:scale-y-[1] hover:before:origin-bottom before:ease-in-out hover:text-[#0B111E] before:transition-transform before:duration-[350ms]' type='button' download>Download CV</button>
+              <a className='text-[max(10px,4.4vw)] min-[450px]:text-[max(20px,3.7vw)] md:text-[max(25px,3.3vw)]  lg:text-[clamp(1rem,0.541rem+0.7172vw,1.4375rem)] w-[max(7.1rem,38vw)] max-w-[185px] min-[450px]:px-[1rem] md:px-[1.8rem] md:py-[.8rem] lg:py-[.6rem] lg:px-[1.1rem] min-[450px]:max-w-fit lg:w-fit lg:max-w-none relative cursor-pointer border-1 text-[#43b0f1] border-[#43b0f1] py-[0.6rem] rounded-xl overflow-hidden z-[1] before:z-[-1]  before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0  before:bg-[#43b0f1] before:scale-y-0 before:origin-top hover:before:scale-y-[1] hover:before:origin-bottom before:ease-in-out hover:text-[#0B111E] before:transition-transform before:duration-[350ms]' href='src/assets/New_Somy_Resume.pdf' type='button' download>Download CV</a>
               <NavLink className="text-[max(10px,4.4vw)] min-[450px]:text-[max(20px,3.7vw)] md:text-[max(25px,3.3vw)] lg:text-[clamp(1rem,0.541rem+0.7172vw,1.4375rem)] bg-[#43b0f1] text-[#0B111E] md:py-[.8rem] py-[0.6rem] lg:py-[0.6rem] px-[max(1rem,6vw)] md:px-[1.8rem] lg:px-[1.5rem] rounded-xl" to="/about">About</NavLink>
             </span>
           </span>
