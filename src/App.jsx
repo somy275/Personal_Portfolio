b5aeefda4318efcf264663fd0c9d7f1304fead4d
somy275 <somyranjank@gmail.com>
2025-11-07 17:52:38 +0530

    optimize app performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import Home from './Pages/Home'
 import Applayout from './components/Applayout'
-import About from './Pages/About'
-import Work from './Pages/Work'
-import Skills from './Pages/Skills'
-import Contact from './Pages/Contact'
+const About=lazy(()=>import('./Pages/About'))
+const Work=lazy(()=>import('./Pages/Work'))
+const Skills=lazy(()=>import("./Pages/skills"))
+const Contact=lazy(()=>import("./Pages/Contact"))
 
 const App = () => {
 
@@ -20,19 +20,31 @@ const App = () => {
         },
         {
           path: '/about',
-          element: <About />
+          element:<Suspense fallback={ <div className='h-screen text-white flex items-center justify-center py-4 font-medium'>
+                <div className='animate-spin   rounded-full h-9 w-9 border-b-2 border-[#43B0F1] mr-2.5'></div>
+                <span className="font-medium text-xl lg:text-xl">loading</span>
+            </div>}> <About /></Suspense>
         },
         {
           path: '/work',
-          element: <Work />
+          element: <Suspense fallback={ <div className='h-screen text-white flex items-center justify-center py-4 font-medium'>
+                <div className='animate-spin   rounded-full h-9 w-9 border-b-2 border-[#43B0F1] mr-2.5'></div>
+                <span className="font-medium text-xl lg:text-xl">loading</span>
+            </div>}> <Work /></Suspense>
         },
         {
           path: '/skills',
-          element: <Skills />
+          element:<Suspense fallback={ <div className='h-screen text-white flex items-center justify-center py-4 font-medium'>
+                <div className='animate-spin   rounded-full h-9 w-9 border-b-2 border-[#43B0F1] mr-2.5'></div>
+                <span className="font-medium text-xl lg:text-xl">loading</span>
+            </div>}> <Skills /></Suspense>
         },
         {
           path: '/contact',
-          element: <Contact />
+          element: <Suspense fallback={ <div className='h-screen text-white flex items-center justify-center py-4 font-medium'>
+                <div className='animate-spin   rounded-full h-9 w-9 border-b-2 border-[#43B0F1] mr-2.5'></div>
+                <span className="font-medium text-xl lg:text-xl">loading</span>
+            </div>}> <Contact /></Suspense>
         }
       ]
     }
