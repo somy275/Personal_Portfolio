48557f33da527c758a011a14be5fcc8065bd2820
somy275 <somyranjank@gmail.com>
2025-11-19 21:53:41 +0530

    some changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React, { lazy, Suspense } from 'react'
+import { lazy, Suspense } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import Home from './Pages/Home'
 import Applayout from './components/Applayout'
-const About=lazy(()=>import('./Pages/About'))
-const Work=lazy(()=>import('./Pages/Work'))
-const Skills=lazy(()=>import("./Pages/skills"))
-const Contact=lazy(()=>import("./Pages/Contact"))
+const About=lazy(()=>import('./Pages/About')) //lazily loaded component, loaded only when needed
+const Work=lazy(()=>import('./Pages/Work')) //lazily loaded component, loaded only when needed
+const Skills=lazy(()=>import("./Pages/Skills")) //lazily loaded component, loaded only when needed
+const Contact=lazy(()=>import("./Pages/Contact")) //lazily loaded component, loaded only when needed
 
 const App = () => {
-
+// Create a router instance 
   const router = createBrowserRouter([
     {
       path: '/',
@@ -23,28 +23,28 @@ const App = () => {
           element:<Suspense fallback={ <div className='h-screen text-white flex items-center justify-center py-4 font-medium'>
                 <div className='animate-spin   rounded-full h-9 w-9 border-b-2 border-[#43B0F1] mr-2.5'></div>
                 <span className="font-medium text-xl lg:text-xl">loading</span>
-            </div>}> <About /></Suspense>
+            </div>}> <About /></Suspense> // It shows loading spinner until component is loaded
         },
         {
           path: '/work',
           element: <Suspense fallback={ <div className='h-screen text-white flex items-center justify-center py-4 font-medium'>
                 <div className='animate-spin   rounded-full h-9 w-9 border-b-2 border-[#43B0F1] mr-2.5'></div>
                 <span className="font-medium text-xl lg:text-xl">loading</span>
-            </div>}> <Work /></Suspense>
+            </div>}> <Work /></Suspense> // It shows loading spinner until component is loaded
         },
         {
           path: '/skills',
           element:<Suspense fallback={ <div className='h-screen text-white flex items-center justify-center py-4 font-medium'>
                 <div className='animate-spin   rounded-full h-9 w-9 border-b-2 border-[#43B0F1] mr-2.5'></div>
                 <span className="font-medium text-xl lg:text-xl">loading</span>
-            </div>}> <Skills /></Suspense>
+            </div>}> <Skills /></Suspense>  // It shows loading spinner until component is loaded
         },
         {
           path: '/contact',
           element: <Suspense fallback={ <div className='h-screen text-white flex items-center justify-center py-4 font-medium'>
                 <div className='animate-spin   rounded-full h-9 w-9 border-b-2 border-[#43B0F1] mr-2.5'></div>
                 <span className="font-medium text-xl lg:text-xl">loading</span>
-            </div>}> <Contact /></Suspense>
+            </div>}> <Contact /></Suspense>   // It shows loading spinner until component is loaded
         }
       ]
     }
